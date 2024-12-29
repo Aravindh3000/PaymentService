1c1ffb3ec60725ea7f5dfd62e715f575a4fc6a59
Aravindh S <Asus@LAPTOP-0QEH5KJF>
2024-12-29 13:49:02 +0530

    added yaml file

--- src/main/java/com/scaler/paymentservice/PaymentServiceApplication.java
+++ src/main/java/com/scaler/paymentservice/PaymentServiceApplication.java
@@ -1,5 +1,6 @@
 package com.scaler.paymentservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
